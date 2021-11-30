ab17583527d373e5d69c7d9c37dbcecdb8de630a
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-11-30 09:16:00 -0300

    Consultando o score através do cpf

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import psycopg2
 con = psycopg2.connect(host='localhost', database='sistema-score',
                        user='postgres', password='postgres')
 cur = con.cursor()
-sql = 'create table usuarios (id serial primary key, cpf varchar(16), score INTEGER NOT NULL, mes varchar(3))'
-cur.execute(sql)
+##sql = 'create table usuarios (id serial primary key, cpf varchar(16), score INTEGER NOT NULL, mes varchar(3))'
+# cur.execute(sql)
 sql = "insert into usuarios values (default,'123.123.123-12', 70, 'AGO' )"
 cur.execute(sql)
 con.commit()
-cur.execute('select * from usuarios')
+cpf = "'123.123.123-12'"
+cur.execute('SELECT score from usuarios WHERE cpf = ' + cpf)
 recset = cur.fetchall()
 for rec in recset:
     print(rec)
