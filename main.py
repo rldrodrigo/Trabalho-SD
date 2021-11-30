49eda3a74552699ae257fa5f08b4ef380be0312a
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-11-30 08:56:30 -0300

    Configuração de conexão com o banco

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 
 
-# configurações do banco
 import psycopg2
-
-
-host = 'localhost'
-dbname = 'teste'
-user = 'postgres'
-password = 'postgres'
-sslmode = 'require'
-
-# string de conexão
-conn_string = 'host={0} user={1} dbname = {2} password = {3} sslmode = {4}'.format(
-    host, user, dbname, password, sslmode)
-
-conn = psycopg2.connect(conn_string)
+con = psycopg2.connect(host='localhost', database='sistema-score',
+                       user='postgres', password='postgres')
+cur = con.cursor()
+sql = 'create table usuarios (id serial primary key, cpf varchar(16), score INTEGER NOT NULL, mes varchar(3))'
+cur.execute(sql)
+sql = "insert into usuarios values (default,'123.123.123-12', 70, 'AGO' )"
+cur.execute(sql)
+con.commit()
+cur.execute('select * from usuarios')
+recset = cur.fetchall()
+for rec in recset:
+    print(rec)
+con.close()
