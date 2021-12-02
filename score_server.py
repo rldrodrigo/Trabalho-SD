5922d332dc20121462c5f725a42515c8b1a9b42e
Cristiano Costa <cristiano@cristianocosta.me>
2021-12-02 13:21:56 -0300

    feat: add dockerfile and env file

--- score_server.py
+++ score_server.py
@@ -1,5 +1,6 @@
+# coding: utf-8
 
-from concurrent import futures
+import os
 import logging
 import psycopg2
 
@@ -7,32 +8,52 @@ import grpc
 import VerificaScore_pb2
 import VerificaScore_pb2_grpc
 
+from concurrent import futures
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PWD = os.getenv('POSTGRES_PWD')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+SERVER_PORT = os.getenv('SERVER_PORT')
+
 
 class Greeter(VerificaScore_pb2_grpc.GreeterServicer):
 
     def SearchScore(self, request, context):
-        con = psycopg2.connect(host='localhost', database='sistema-score',
-                               user='postgres', password='postgres')
+        print("[score-server]: SearchScore do CPF " + request.cpf + " via gRPC")
+
+        con = psycopg2.connect(host=POSTGRES_HOST, database=POSTGRES_DB,
+                               user=POSTGRES_USER, password=POSTGRES_PWD,
+                               port=POSTGRES_PORT)
         cur = con.cursor()
+
         # Comandos para criação e inserção de dados no Postgres logo na instancia do server
         #sql = 'create table usuarios (id serial primary key, cpf varchar(16), score INTEGER NOT NULL, mes varchar(3))'
         # cur.execute(sql)
         #sql = "insert into usuarios values (default,'12312312383', 70, 'AGO' )"
         # cur.execute(sql)
+
         con.commit()
         cur.execute("SELECT * from usuarios WHERE cpf = '" +
                     request.cpf+"' AND mes = '" + request.mes+"' ")
         recset = cur.fetchall()
         for rec in recset:
             scoreRetorno = str(rec[2])
-            return VerificaScore_pb2.ScoreReply(score="O Score do CPF: " + request.cpf + " é de: "+scoreRetorno)
+            con.close()
+            return VerificaScore_pb2.ScoreReply(score=scoreRetorno)
+
         con.close()
+        return VerificaScore_pb2.ScoreReply(score="0")
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     VerificaScore_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('[::]:' + SERVER_PORT)
     server.start()
     server.wait_for_termination()
 
